3760a3380d93f5de6cecfafa622daf1fcee09ee3
sxntiagoad <salvarezd3@eafit.edu.co>
2024-09-28 21:14:18 -0500

    updated

--- lib/core/excel/excel_generator.dart
+++ lib/core/excel/excel_generator.dart
@@ -1,16 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva/providers/current_user_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../../models/preoperacional.dart';
 import '../../providers/car_provider.dart';
 import '../../providers/endpoint.dart';
 
-Future<void> dataJson({
-  required WidgetRef ref,
-  required Preoperacional preoperacional,
-  String idDoc=''
-}) async {
+Future<void> dataJson(
+    {required WidgetRef ref,
+    required Preoperacional preoperacional,
+    String idDoc = ''}) async {
   Map<String, Map<String, Object>> data = {};
 
   preoperacional.inspecciones.forEach((key, value) {
@@ -33,15 +33,30 @@ Future<void> dataJson({
     "F.V . TECNICOMECANICA": fecha(currentCar?.tecnicoMec),
     "F.V . EXTRACTO": fecha(currentCar?.extracto),
     "BOTIQUÍN TIPO": preoperacional.typeKit,
-    "SEMANA DEL": "",
-    "AL": "",
-    "DEL 20": "",
-    "KMTS INICIAL": "",
-    "KMTS FINAL": "",
-    "KMTS ÚLTIMO CAMBIO DE ACEITE": "",
-    "KMTS PROXIMO CAMBIO DE ACEITE": ""
+    "SEMANA DEL": formatDate(preoperacional.fechaInit),
+    "AL": formatDate(preoperacional.fechaFinal),
+    "DEL 20": "24",
+    "KMTS INICIAL": preoperacional.kilometrajeInit.toString(),
+    "KMTS FINAL": preoperacional.kilometrajeFinal.toString(),
+    "KMTS ÚLTIMO CAMBIO DE ACEITE": fecha(currentCar?.ultCambioAceite),
+    "KMTS PROXIMO CAMBIO DE ACEITE": fecha(currentCar?.proxCambioAceite),
   };
   final user = ref.read(currentUserProvider);
+  final userValue = user.value;
+
+  final logoUrl = await getSESCOTURImageUrl();
+  String firmaUrl = '';
+  if (userValue != null) {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      firmaUrl = await getUserSignatureUrl(uid);
+    }
+  }
+  data['IMAGENES'] = {
+    'FIRMA_USER': firmaUrl,
+    'FIRMA_ENCARGADO': user.value?.fullName ?? '',
+    'LOGO': logoUrl,
+  };
 
   data["PIE_TABLA"] = {
     "Nombre del Conductor": user.value?.fullName ?? '',
@@ -52,7 +67,7 @@ Future<void> dataJson({
 
   await enviarJsonYSubirArchivoAFirebase(
     jsonData: data,
-    archiveName: preoperacional.docId.isEmpty?idDoc:preoperacional.docId,
+    archiveName: preoperacional.docId.isEmpty ? idDoc : preoperacional.docId,
   );
 }
 
@@ -69,5 +84,43 @@ String fecha(Timestamp? fecha) {
     String formattedDate = '$year-$month-$day $hour:$minute';
     return formattedDate;
   }
-  return 'no hay fecha';
+  return 'Sin fecha';
+}
+
+String formatDate(String? fecha) {
+  if (fecha == null || fecha.isEmpty) return 'Sin fecha';
+  try {
+    DateTime dateTime = DateTime.parse(fecha);
+    String dia = dateTime.day.toString().padLeft(2, '0');
+    String mes = dateTime.month.toString().padLeft(2, '0');
+    String hora = dateTime.hour.toString().padLeft(2, '0');
+    String minutos = dateTime.minute.toString().padLeft(2, '0');
+
+    return '$dia/$mes $hora:$minutos';
+  } catch (e) {
+    return 'Fecha inválida';
+  }
+}
+
+Future<String> getSESCOTURImageUrl() async {
+  try {
+    final storage = FirebaseStorage.instance;
+    final ref = storage.ref().child('SESCOTUR.png');
+    final url = await ref.getDownloadURL();
+    return url;
+  } catch (e) {
+    // ignore: avoid_print
+    print('Error al obtener la URL de SESCOTUR.png: $e');
+    return ''; // Retorna una cadena vacía en caso de error
+  }
+}
+
+Future<String> getUserSignatureUrl(String uid) async {
+  try {
+    return await FirebaseStorage.instance.ref('firmas/$uid.png').getDownloadURL();
+  } catch (e) {
+    // ignore: avoid_print
+    print('Error al obtener la firma del usuario: $e');
+    return '';
+  }
 }
